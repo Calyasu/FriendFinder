6d4ff4252f42f75f46d1ebf8297cba343863ee55
Calvin <calvinctvodesign@gmail.com>
2019-07-25 22:11:03 -0700

    semi-done

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+var friends = [
+    {
+        name: "Y'shtola",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj-t7fNrM_jAhXxOn0KHSnaD9wQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F367747125822853393%2F&psig=AOvVaw2-5eaKGIC6dgcwxmqYKCpi&ust=1564119294003102",
+        score:[
+            "2",
+            "4",
+            "4",
+            "1",
+            "3",
+            "1",
+            "4",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Thancred",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwja4dLQrs_jAhVVJDQIHcRACWsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fffxiv%2Fcomments%2Fbuf6su%2Fhires_shadowbringers_cg_art_thancred%2F&psig=AOvVaw3-q_q_T57UIjMGBaCCOU_W&ust=1564119842220048",
+        score:[
+            "1",
+            "3",
+            "3",
+            "1",
+            "4",
+            "1",
+            "3",
+            "2",
+            "1",
+            "3"
+        ]
+    },
+    {
+        name: "Alphinaud",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi459X4r8_jAhXgCjQIHUPIDNAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pictosee.com%2Ftag%2Falphinaudleveilleur&psig=AOvVaw0Ik7V4Oc8CqHtstQCsW3ZI&ust=1564120178124251",
+        score:[
+            "4",
+            "1",
+            "2",
+            "4",
+            "4",
+            "3",
+            "4",
+            "1",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Alisaie",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjAg4iJsc_jAhUZGTQIHbtxC6wQjRx6BAgBEAU&url=https%3A%2F%2Fboards.fireden.net%2Fv%2Fthread%2F449460075%2F&psig=AOvVaw0CuwLjwPNqjOHjP0L6HPkD&ust=1564120482607463",
+        score:[
+            "2",
+            "3",
+            "4",
+            "1",
+            "2",
+            "4",
+            "2",
+            "2",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name: "Urianger",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj8wdG0ss_jAhWNGTQIHa1_DdAQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Fhashtag%2Furianger&psig=AOvVaw31SjtfU9OU8_b6NBONDOam&ust=1564120797592629",
+        score:[
+            "1",
+            "1",
+            "2",
+            "4",
+            "2",
+            "4",
+            "3",
+            "4",
+            "4",
+            "1"
+        ]
+    }
+];
+module.exports = friends;
